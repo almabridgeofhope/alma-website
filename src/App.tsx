a5f8ab56415acb3f80f93c467c05b0bc07e7605c
Clara <clara.thue@gmail.com>
2025-11-18 12:40:36 +0100

    Add future configuration options for React Router in App component

--- src/App.tsx
+++ src/App.tsx
@@ -141,6 +141,10 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter
+            future={{
+              v7_startTransition: true,
+              v7_relativeSplatPath: true,
+            }}
             basename="/"
           >
             <Handle404Redirect />
